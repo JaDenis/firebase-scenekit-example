4c2dcab58c78185a84db329a9c16e8be33ab5c30
Maximilian Alexander <max@zinkpulse.com>
2015-07-20 02:13:26 -0700

    adding new sprite position

--- FirebaseSceneKitExample/GameScene.swift
+++ FirebaseSceneKitExample/GameScene.swift
@@ -9,34 +9,38 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    override init() {
+        super.init()
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+    }
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        self.backgroundColor = UIColor.blackColor()
         
-        self.addChild(myLabel)
+        addSpaceShipSprite(CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame)))
+        
+    }
+    
+    func addSpaceShipSprite(position: CGPoint){
+        let sprite = SKSpriteNode(imageNamed:"Spaceship")
+        
+        sprite.xScale = 0.2
+        sprite.yScale = 0.2
+        sprite.position = position
+        
+        
+        self.addChild(sprite);
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
